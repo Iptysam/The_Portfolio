51f909ce4134443df379a233882e4d4bec3dda78
Iptysam <mukadamiptisam@gmail.com>
2022-09-06 15:54:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const hamburger = document.querySelector('.hamburger');
 const Menu = document.querySelector('.menu');
 const navlinks = document.querySelectorAll(".nav-link");
 const closeBtn = document.querySelector(".close_btn");
-
+console.log(closeBtn);
 
 
 function open() {
     hamburger.classList.toggle('active');
     Menu.classList.toggle("active");
 }
-
+/** 
 function close() {
+    console.log('working');
     hamburger.classList.remove('active');
     Menu.classList.remove('active');
 }
-
+**/
 hamburger.addEventListener("click", open);
-closeBtn.addEventListener("click", close);
+closeBtn.addEventListener("click", open);
