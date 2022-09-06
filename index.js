83b7c8165abfe73d6d6b1d24eb71aac08d3f98dd
Iptysam <mukadamiptisam@gmail.com>
2022-09-06 16:26:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ function open() {
     hamburger.classList.toggle('active');
     Menu.classList.toggle("active");
 }
-/** 
+/**
 function close() {
     console.log('working');
     hamburger.classList.remove('active');
     Menu.classList.remove('active');
-}
-**/
+}**/
+
 hamburger.addEventListener("click", open);
 closeBtn.addEventListener("click", open);
+navlinks.forEach(link => {
+  link.addEventListener('click', open);   
+});
