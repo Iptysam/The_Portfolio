e5209ec16bb65633de4ea01156565c337b4ffa6f
Iptysam <mukadamiptisam@gmail.com>
2022-09-07 14:49:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,56 +14,71 @@ navlinks.forEach((link) => {
   link.addEventListener('click', open);
 });
 
+function popup() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
 const allCards = [{
-  id: 'put an id here like 1 or c-1',
-  title: 'place the title here',
+  id: 'card1',
+  title: 'Tonic',
   feature: ['Canopy', 'Back End Dev', 2015],
   sdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
   ldescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  liveLink: 'live link to your portfolio',
-  cardImage: 'place your path of the image here',
-  dotImg: 'the path of the small dot in between the canopy and the backend dev',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  cardImage: 'Images/SnapshootPortfolio.png',
+  dotImg: 'Images/Counter.png',
   btags: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstraps'],
   stags: ['html', 'css', 'javascript'],
-  class: 'cla-1',
+  class: 'container1',
 },
 {
-  id: 'put an id here like 1 or c-1',
-  title: 'place the title here',
+  id: 'card2',
+  title: 'Multi-Post Stories',
   feature: ['Canopy', 'Back End Dev', 2015],
   sdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
   ldescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  liveLink: 'live link to your portfolio',
-  cardImage: 'place your path of the image here',
-  dotImg: 'the path of the small dot in between the canopy and the backend dev',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  cardImage: 'Images/SnapshootPortfolio3.png',
+  dotImg: 'Images/Counter.png',
   btags: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstraps'],
   stags: ['html', 'css', 'javascript'],
-  class: 'cla-1',
+  class: 'container2',
 },
 {
-  id: 'put an id here like 1 or c-1',
-  title: 'place the title here',
+  id: 'card3',
+  title: 'Tonic',
   feature: ['Canopy', 'Back End Dev', 2015],
   sdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
   ldescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  liveLink: 'live link to your portfolio',
-  cardImage: 'place your path of the image here',
-  dotImg: 'the path of the small dot in between the canopy and the backend dev',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  cardImage: 'Images/SnapshootPortfolio1.png',
+  dotImg: 'Images/Counter.png',
   btags: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstraps'],
   stags: ['html', 'css', 'javascript'],
-  class: 'cla-1',
+  class: 'container3',
 },
 {
-  id: 'put an id here like 1 or c-1',
-  title: 'place the title here',
+  id: 'card4',
+  title: 'Multi-post Stories',
   feature: ['Canopy', 'Back End Dev', 2015],
   sdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy',
   ldescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  liveLink: 'live link to your portfolio',
-  cardImage: 'place your path of the image here',
-  dotImg: 'the path of the small dot in between the canopy and the backend dev',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  cardImage: 'Images/SnapshootPortfolio2.png',
+  dotImg: 'Images/Counter.png',
   btags: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstraps'],
   stags: ['html', 'css', 'javascript'],
-  class: 'cla-1',
+  class: 'container4',
 },
 ]
+
+const ids = arr.map(({ id }) => {
+  return id;
+});
+
+function openForm() {
+  var popup= document.getElementById("card1");
+  popup.classList.toggle("show");
+}
+
